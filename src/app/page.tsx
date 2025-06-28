b0a881fda4009d552a402e6051ec89f0e68339d2
Paweł Gałązka <829242+pawelgalazka@users.noreply.github.com>
2025-06-28 11:47:42 +1000

    Request graphql api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,34 @@
-import Image from "next/image"
-import { Button, HStack } from "@chakra-ui/react"
+"use client"
+
+import { useQuery, gql } from "@apollo/client"
+
+import { Button, HStack, Image, Text } from "@chakra-ui/react"
+
+const GET_CHARACTERS = gql`
+  query GetCharacters {
+    characters {
+      results {
+        name
+        image
+      }
+    }
+  }
+`
 
 export default function Home() {
+  const { data, loading, error } = useQuery(GET_CHARACTERS)
+
+  if (loading) return <Text>Loading...</Text>
+  if (error) return <Text>Error: {error.message}</Text>
+
   return (
     <HStack>
-      <Button>Click me</Button>
-      <Button>Click me</Button>
+      {data.characters.results.map((character: any) => (
+        <HStack key={character.id}>
+          <Image src={character.image} alt={character.name} />
+          <Text>{character.name}</Text>
+        </HStack>
+      ))}
     </HStack>
   )
 }
