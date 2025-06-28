a88589ba0549facb9982741365dbe19e209f2a07
Paweł Gałązka <829242+pawelgalazka@users.noreply.github.com>
2025-06-28 19:41:28 +1000

    Refine user form

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,6 +1,7 @@
 import { UserForm } from "@/components/user-form"
 import { getUserDetails } from "@/actions/user"
 import { Metadata } from "next"
+import { Container } from "@chakra-ui/react"
 
 export const metadata: Metadata = {
   title: "User Details",
@@ -9,5 +10,9 @@ export const metadata: Metadata = {
 
 export default async function User() {
   const userDetails = await getUserDetails()
-  return <UserForm userDetails={userDetails} />
+  return (
+    <Container>
+      <UserForm userDetails={userDetails} />
+    </Container>
+  )
 }
